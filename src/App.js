079f2410e7c683a2b669b56d4215124dd061ba2c
Gede Kresna <gedekresnap@gmail.com>
2021-09-27 11:29:46 +0700

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-   <h2>I Gede Kresna </h2>
+  <Navbar/>
   );
 }
 
